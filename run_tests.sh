3db8ca01a6944677fc7e19d548fc7ccb246a6068
Tanya <tanyakovalenko1994@gmail.com>
2015-12-04 10:50:50 -0500

    Change run_tests.sh

--- run_tests.sh
+++ run_tests.sh
@@ -1,7 +1,10 @@
-cd $WORKSPACE 
-./test_lab_1_1.sh
-./test_lab_1_2.sh
-./test_lab_1_3_1.sh
-./test_lab_1_3_2.sh
-./test_lab_1_3_3.sh
-./test_lab_1_3_4.sh
+git config user.name "Builder"
+git config user.email "builder@localhost"
+git merge --no-commit origin1/c++-2016
+cd c++/task-implementation/src
+"$WORKSPACE"/test_lab_1_1.sh
+"$WORKSPACE"/test_lab_1_2.sh
+"$WORKSPACE"/test_lab_1_3_1.sh
+"$WORKSPACE"/test_lab_1_3_2.sh
+"$WORKSPACE"/test_lab_1_3_3.sh
+"$WORKSPACE"/test_lab_1_3_4.sh
