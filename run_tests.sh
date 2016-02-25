c7cc9c61c60b182fc6ba52ce039ae6232819b088
Tanya <tanyakovalenko1994@gmail.com>
2016-02-25 10:30:41 -0500

    add report file

--- run_tests.sh
+++ run_tests.sh
@@ -1,11 +1,32 @@
-(git config user.name "Builder") > results
-(git config user.email "builder@localhost") > results
-(git merge --no-commit origin1/c++-2016) > results
+git config user.name "Builder"
+git config user.email "builder@localhost"
+git merge --no-commit origin1/c++-2016
 cd c++/task-implementation/src
+echo "
+LAB 1.1
+"
 "$WORKSPACE"/test_lab_1_1.sh
+echo "
+LAB 1.2:
+"
 "$WORKSPACE"/test_lab_1_2.sh
+echo "
+LAB 1.3.1:
+"
 "$WORKSPACE"/test_lab_1_3_1.sh
+echo "
+LAB 1.3.2:
+"
 "$WORKSPACE"/test_lab_1_3_2.sh
+echo "
+LAB 1.3.3:
+"
 "$WORKSPACE"/test_lab_1_3_3.sh
+echo "
+LAB 1.3.4:
+"
 "$WORKSPACE"/test_lab_1_3_4.sh
+echo "
+LAB 1.4:
+"
 "$WORKSPACE"/test_lab_1_4.sh
