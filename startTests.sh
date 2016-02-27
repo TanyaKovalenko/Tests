98362b113c812ea099b3fe438dcd17267c5869d4
Tanya <tanyakovalenko1994@gmail.com>
2016-02-27 07:24:18 -0500

    Fix

--- startTests.sh
+++ startTests.sh
@@ -1,5 +1,5 @@
 touch $WORKSPACE/results.html
 cat $WORKSPACE/report.html > $WORKSPACE/results.html 
-valgrind --tool=cachegrind $WORKSPACE/run_tests.sh | awk 'BEGIN{print "<table BORDER=0 CELLSPACING=2 CELLPADDING=2> <caption> <b> <br> Test results: <br> </b> </caption>"} {print "<tr>";print "<td>" $0"</td>";print "</tr>"} END{print "</table>"}' >> $WORKSPACE/results.html 
+valgrind --tool=massif $WORKSPACE/run_tests.sh | awk 'BEGIN{print "<table BORDER=0 CELLSPACING=2 CELLPADDING=2> <caption> <b> <br> Massif tool: <br> </b> </caption>"} {print "<tr>";print "<td>" $0"</td>";print "</tr>"} END{print "</table>"}' >> $WORKSPACE/results.html 
 echo "<br> <b> VALGRIND REPORT: </b>" >> $WORKSPACE/results.html 
-valgrind --tool=massif $WORKSPACE/run_tests.sh 2>&1 >/dev/null | awk 'BEGIN{print "<table BORDER=0 CELLSPACING=2 CELLPADDING=2> <caption> <b> <br> Massif tool: <br> </b> </caption>"} {print "<tr>";print "<td>" $0"</td>";print "</tr>"} END{print "</table>"}' >> $WORKSPACE/results.html
+valgrind --tool=cachegrind $WORKSPACE/run_tests.sh 2>&1 >/dev/null | awk 'BEGIN{print "<table BORDER=0 CELLSPACING=2 CELLPADDING=2> <caption> <b> <br> Test results: <br> </b> </caption>"} {print "<tr>";print "<td>" $0"</td>";print "</tr>"} END{print "</table>"}' >> $WORKSPACE/results.html
