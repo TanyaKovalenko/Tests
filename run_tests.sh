6483ff726d0a73d01e0972893319e186f058abd9
Tanya <tanyakovalenko1994@gmail.com>
2016-03-01 07:30:59 -0500

    fix

--- run_tests.sh
+++ run_tests.sh
@@ -3,6 +3,6 @@ git config user.email "builder@localhost"
 git merge --no-commit origin1/c++-2016
 cd c++/task-implementation/src
 echo "
-LAB 1.3.2:
+LAB 1.3.4:
 "
-"$WORKSPACE"/test_lab_1_3_2.sh
+"$WORKSPACE"/test_lab_1_3_4.sh
