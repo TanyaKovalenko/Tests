a4ba69688ca78b48e03b5da2d5d8f06c7b1e605a
Tanya <tanyakovalenko1994@gmail.com>
2016-03-03 15:49:04 -0500

    change sed

--- startTests.sh
+++ startTests.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-cp report.html results.html 
-echo "<br> <b> VALGRIND REPORT: </b>" >> results.html
+cp report.html results.html
 ./run_tests.sh > tests.out 2>&1
 sed -e 's/&/&amp/g' tests.out > tests1.out
 cat tests1.out > tests.out
@@ -12,6 +11,7 @@ sed -e 's/</&lt/g' tests.out > tests1.out
 cat tests1.out > tests.out
 sed -e 's/>/&gt/g' tests.out > tests1.out
 cat tests1.out > tests.out
+echo "<br> <b> VALGRIND REPORT: </b>" >> results.html
 awk 'BEGIN{print "<table BORDER=0 CELLSPACING=2 CELLPADDING=2> <caption> <b> <br> Tests results: <br> </b> </caption>"} {print "<tr>";print "<td>" $0"</td>";print "</tr>"} END{print "</table>"}' < tests.out >> results.html
 for arg in _1_1 _1_2 _1_3_1 _1_3_2 _1_3_3 _1_3_4 _1_4
 do
