43d4c51a3c9b15848f2c928d4d9c970f00fd04fb
Tanya <tanyakovalenko1994@gmail.com>
2016-03-03 15:19:23 -0500

    fix

--- startTests.sh
+++ startTests.sh
@@ -2,12 +2,11 @@
 cp report.html results.html 
 echo "<br> <b> VALGRIND REPORT: </b>" >> results.html
 ./run_tests.sh > tests.out 2>&1
-cat tests.out
-sed -e 's/&/&amp/g' tests.out > tests.out
-sed -e 's/"/&quot/g' tests.out > tests.out
-sed -e "s/'/&apos/g" tests.out > tests.out
-sed -e 's/</&lt/g' tests.out > tests.out
-sed -e 's/>/&gt/g' tests.out > tests.out
+sed -e 's/&/&amp/g' tests.out
+sed -e 's/"/&quot/g' tests.out
+sed -e "s/'/&apos/g" tests.out
+sed -e 's/</&lt/g' tests.out
+sed -e 's/>/&gt/g' tests.out
 cat tests.out
 awk 'BEGIN{print "<table BORDER=0 CELLSPACING=2 CELLPADDING=2> <caption> <b> <br> Memcheck tool: <br> </b> </caption>"} {print "<tr>";print "<td>" $0"</td>";print "</tr>"} END{print "</table>"}' < tests.out >> results.html
 for arg in _1_1 _1_2 _1_3_1 _1_3_2 _1_3_3 _1_3_4 _1_4
