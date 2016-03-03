c7bb35cbe505fd102548daabf946eb8e6d022e75
Tanya <tanyakovalenko1994@gmail.com>
2016-03-03 15:06:15 -0500

    fix

--- startTests.sh
+++ startTests.sh
@@ -7,11 +7,12 @@ sed -e 's/"/&quot/g' tests.out > tests.out
 sed -e "s/'/&apos/g" tests.out > tests.out
 sed -e 's/</&lt/g' tests.out > tests.out
 sed -e 's/>/&gt/g' tests.out > tests.out
-awk 'BEGIN{print "<table BORDER=0 CELLSPACING=2 CELLPADDING=2> <caption> <b> <br> Memcheck tool: <br> </b> </caption>"} {print "<tr>";print "<td>" $0"</td>";print "</tr>"} END{print "</table>"}' < tests.out >> $WORKSPACE/results.html
+cat tests.out
+awk 'BEGIN{print "<table BORDER=0 CELLSPACING=2 CELLPADDING=2> <caption> <b> <br> Memcheck tool: <br> </b> </caption>"} {print "<tr>";print "<td>" $0"</td>";print "</tr>"} END{print "</table>"}' < tests.out >> results.html
 for arg in _1_1 _1_2 _1_3_1 _1_3_2 _1_3_3 _1_3_4 _1_4
 do
 	htmlFile=memcheckRes$arg.html
-    xmlFile=memcheckRes$arg.xml
+    	xmlFile=memcheckRes$arg.xml
 	xsltproc -o $htmlFile style.xsl $xmlFile
-    cat $htmlFile >> results.html
+    	cat $htmlFile >> results.html
 done
