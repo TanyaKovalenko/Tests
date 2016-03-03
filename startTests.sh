f48835930c9b759cd47301fd36236531a941f92a
Tanya <tanyakovalenko1994@gmail.com>
2016-03-03 15:39:39 -0500

    change sed

--- startTests.sh
+++ startTests.sh
@@ -12,6 +12,7 @@ sed -e 's/</&lt/g' tests.out > tests1.out
 cat tests1.out > tests.out
 sed -e 's/>/&gt/g' tests.out > tests1.out
 cat tests1.out > tests.out
+cat tests.out
 awk 'BEGIN{print "<table BORDER=0 CELLSPACING=2 CELLPADDING=2> <caption> <b> <br> Memcheck tool: <br> </b> </caption>"} {print "<tr>";print "<td>" $0"</td>";print "</tr>"} END{print "</table>"}' < tests.out >> results.html
 for arg in _1_1 _1_2 _1_3_1 _1_3_2 _1_3_3 _1_3_4 _1_4
 do
