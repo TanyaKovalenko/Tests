8350ec06c3411820815c1f6a663b47d0f6f2d2f5
Tanya <tanyakovalenko1994@gmail.com>
2016-04-05 11:30:52 -0400

    Change curl request

--- script.sh
+++ script.sh
@@ -22,4 +22,5 @@ do
             	fi
         done
     done
+    curl -H "PRIVATE-TOKEN: SfkWoPZmXuzEAEa-XW-4" --data "@result.html" -X POST https://gitlab.com/api/v3/projects/939427/merge_requests/$ID/notes
 done
