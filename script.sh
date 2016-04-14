1d1a410ff4683d91ba2315e537873c7ec72a9a60
Tanya <tanyakovalenko1994@gmail.com>
2016-04-14 16:28:53 -0400

    Add static analysis tool cppcheck

--- script.sh
+++ script.sh
@@ -12,14 +12,27 @@ do
     do
     	numOfChange=$(($numOfChange-1))
         cat changes.json | jq ".changes[$numOfChange].new_path" > path_file
+	indexOfLab = 0
         for t in `seq 1 8`
         do
 		RES=$(grep -c /$t/ path_file)     
 		if [ $RES -ne "0" ]
             	then
-                	./startTests$t.sh
-    			curl -H "PRIVATE-TOKEN: SfkWoPZmXuzEAEa-XW-4" -F body="<results$t.html" -X POST https://gitlab.com/api/v3/projects/939427/merge_requests/$ID/notes
+                	input_array[$indexOfLab]=$t
         	fi
+
+	done
+	for l in input_array
+                do
+                        cppcheck path_file --xml 2> cppcheck-result$l.xml
+                        ./startTests$l.sh
+                        htmlFile=cppcheck-result$l.html
+                        xmlFile=cppcheck-result$l.xml
+                        cat cppcheck-result$l.xml
+			#xsltproc -o $htmlFile style.xsl $xmlFile
+                        #echo "<br> <b> LAB$l: </b> <br>" >> results1.html
+                        #cat $htmlFile >> results1.html
+                        curl -H "PRIVATE-TOKEN: SfkWoPZmXuzEAEa-XW-4" -F body="<results$l.html" -X POST https://gitlab.com/api/v3/projects/939427/merge_requests/$ID/notes
 	done
     done
 done
