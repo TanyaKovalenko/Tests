a34af969768adeced820d3c5718152830a93f905
Tanya <tanyakovalenko1994@gmail.com>
2016-05-03 17:11:29 -0400

    Fix

--- script.sh
+++ script.sh
@@ -5,17 +5,12 @@ for numOfMR in `seq 1 $LENGTH`
 do
     indexOfMR=$(($numOfMR-1))
     ID=$(cat opened_merge_requests.json | jq ".[$indexOfMR].id")
-	echo $ID
     curl -H "PRIVATE-TOKEN: SfkWoPZmXuzEAEa-XW-4" https://gitlab.com/api/v3/projects/939427/merge_requests/$ID/changes > changes.json
 	LENGTH=$(cat changes.json | jq '.changes | length')
-	cd out
-	ls 
-	cd ../
     for numOfChange in `seq 1 $LENGTH`
 		do
 			numOfChange=$(($numOfChange-1))
 			cat changes.json | jq ".changes[$numOfChange].new_path" > path_file
-			echo $path_file
 			indexOfLab=0
 			for t in `seq 1 8`
 				do
@@ -31,7 +26,8 @@ do
 							path_in_quotes=$(cat path_file)
 							path=$(echo $path_in_quotes | tr -d \")							
 							lab_file=$WORKSPACE/$path
-							echo $lab_file | sed -r "s/\/$l\/.+//"
+							echo $lab_file | sed -r "s/\/$l\/.+//" > path_to_lab_folder
+							lab_file=$(cat path_to_lab_folder)
 							lab_file=$(echo $lab_file/$l/)
 							echo $lab_file
 							cppcheck $lab_file --xml 2> cppcheck-result$l.xml						
