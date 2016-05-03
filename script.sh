84911d3505d8ae37b5e793162a657dcfe673694c
Tanya <tanyakovalenko1994@gmail.com>
2016-05-03 10:10:35 -0400

    Fix

--- script.sh
+++ script.sh
@@ -8,38 +8,35 @@ do
 	echo $ID
     curl -H "PRIVATE-TOKEN: SfkWoPZmXuzEAEa-XW-4" https://gitlab.com/api/v3/projects/939427/merge_requests/$ID/changes > changes.json
 	LENGTH=$(cat changes.json | jq '.changes | length')
+	cd out
+	ls 
+	cd ../
     for numOfChange in `seq 1 $LENGTH`
-    do
-    	numOfChange=$(($numOfChange-1))
-        cat changes.json | jq ".changes[$numOfChange].new_path" > path_file
-	indexOfLab=0
-        for t in `seq 1 8`
-        do
-		RES=$(grep -c /$t/ path_file)     
-		if [ $RES -ne "0" ]
-            	then
-                	input_array[$indexOfLab]=$t
-        	fi
-
-	done
-	ls
-	for l in "${input_array[@]}"
-                do			
-                        ./startTests$l.sh
-						path_in_quotes=$(cat path_file)
-						path=$(echo $path_in_quotes | tr -d \")
-						lab_file=$WORKSPACE/$path
-						cat $lab_file
-                        cppcheck $lab_file --xml 2> cppcheck-result$l.xml
-						#htmlreport/cppcheck-htmlreport --file=cppcheck-result$l.xml --report-dir=. --source-dir=.
-                        htmlFile=cppcheck-result$l.html
-                        xmlFile=cppcheck-result$l.xml
-                        cat cppcheck-result$l.xml
-						xsltproc -o $htmlFile cppcheck.xsl $xmlFile
-						cat $htmlFile 
-                        echo "<br> <b> LAB$l: </b> <br>" >> results1.html
-                        cat $htmlFile >> results1.html
-                        curl -H "PRIVATE-TOKEN: SfkWoPZmXuzEAEa-XW-4" -F body="<results$l.html" -X POST https://gitlab.com/api/v3/projects/939427/merge_requests/$ID/notes
-	done
+		do
+			numOfChange=$(($numOfChange-1))
+			cat changes.json | jq ".changes[$numOfChange].new_path" > path_file
+			indexOfLab=0
+			for t in `seq 1 8`
+				do
+				RES=$(grep -c /$t/ path_file)     
+				if [ $RES -ne "0" ]
+					then
+						input_array[$indexOfLab]=$t				
+				fi				
+			done
+		for l in "${input_array[@]}"
+					do			
+							./startTests$l.sh
+							path_in_quotes=$(cat path_file)
+							path=$(echo $path_in_quotes | tr -d \")
+							lab_file=$WORKSPACE/$path
+							cppcheck $lab_file --xml 2> cppcheck-result$l.xml						
+							htmlFile=cppcheck-result$l.html
+							xmlFile=cppcheck-result$l.xml
+							xsltproc -o $htmlFile cppcheck.xsl $xmlFile
+							echo "<br> <b> LAB$l: </b> <br>" >> results1.html
+							cat $htmlFile >> results1.html
+							curl -H "PRIVATE-TOKEN: SfkWoPZmXuzEAEa-XW-4" -F body="<results$l.html" -X POST https://gitlab.com/api/v3/projects/939427/merge_requests/$ID/notes
+		done
     done
 done
