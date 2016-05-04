d0d1dc041b7cc142d51c481432825d9f1774e3dc
Tanya <tanyakovalenko1994@gmail.com>
2016-05-04 10:57:21 -0400

    Fix

--- run_tests.sh
+++ run_tests.sh
@@ -1,43 +1,43 @@
 git config user.name "Builder"
 git config user.email "builder@localhost"
 git merge --no-commit origin1/buggy-implementation
-tput bold
+tput -T linux bold
 echo "
 LAB 1.1:
 "
 tput sgr0
 "$WORKSPACE"/test_lab_1_1.sh
-tput bold
+tput -T linux bold
 echo "
 LAB 1.2:
 "
 tput sgr0
 "$WORKSPACE"/test_lab_1_2.sh
-tput bold
+tput -T linux bold
 echo "
 LAB 1.3.1:
 "
 tput sgr0
 "$WORKSPACE"/test_lab_1_3_1.sh
-tput bold
+tput -T linux bold
 echo "
 LAB 1.3.2:
 "
 tput sgr0
 "$WORKSPACE"/test_lab_1_3_2.sh
-tput bold
+tput -T linux bold
 echo "
 LAB 1.3.3:
 "
 tput sgr0
 "$WORKSPACE"/test_lab_1_3_3.sh
-tput bold
+tput -T linux bold
 echo "
 LAB 1.3.4:
 "
 tput sgr0
 "$WORKSPACE"/test_lab_1_3_4.sh
-tput bold
+tput -T linux bold
 echo "
 LAB 1.4:
 "
